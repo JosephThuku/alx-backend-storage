567459dec4c16c2d843d1d7d27904be48af872ee
JosephThuku <jthuku490@gmail.com>
2023-07-13 07:34:00 +0300

    Best band ever!

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- Rank country origin of bands
+-- ordered by the number of fans
+-- Rank country origins of bands by number of fans
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
